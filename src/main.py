b130fcdc2615ea16f8382078bb252e4dd7ec4b41
Luka Zivko <luka.zivko9@gmail.com>
2022-01-02 18:36:35 +0100

    My own version of the main using the tutorial and references.

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+'''
+#CREATED BY KLEMEN
 # import the pygame module, so you can use it
 import pygame
 
@@ -46,3 +48,90 @@ def main():
 if __name__ == "__main__":
     # call the main function
     main()
+'''
+
+#importing pygame so we use its rulesets
+import pygame
+
+#set of rules for the canvas of the game and other
+pygame.init()
+
+pygame.display.set_caption('Snakey')
+
+clock = pygame.time.Clock()
+
+TOTAL_COLS = 60 #columns
+TOTAL_ROWS = 40 #rows
+BLOCK_SIZE = 10 #in other words 10px
+
+SURFACE = pygame.display.set_mode([TOTAL_COLS * BLOCK_SIZE, TOTAL_ROWS * BLOCK_SIZE])
+
+#drawing the snakes head being rectangular
+def draw_single_block(BLOCK_POSITION, COLOR):
+    x = BLOCK_POSITION[0] * BLOCK_SIZE
+    y = BLOCK_POSITION[1] * BLOCK_SIZE
+    pygame.draw.rect(SURFACE, COLOR, (x, y, BLOCK_SIZE, BLOCK_SIZE))
+
+#which direction will the block move when the "user" presses a key
+def detect_direction (direction, event):
+    if(event.type == pygame.KEYDOWN):
+        if event.key == pygame.K_LEFT:
+            return "LEFT"
+        elif event.key == pygame.K_RIGHT:
+            return "RIGHT"
+        elif event.key == pygame.K_UP:
+            return "UP"
+        elif event.key == pygame.K_DOWN:
+            return "DOWN"
+    return direction;
+
+#this definition is for the head of the snake to move when pressing a directional key
+def move_block(column, row, direction):
+    if direction == "LEFT":
+        column = column - 1
+    if direction == "RIGHT":
+        column = column + 1
+    if direction == "UP":
+        row = row - 1
+    if direction == "DOWN":
+        row = row + 1
+    return (column, row)
+
+#this defintion was created first to create the window and to add other created definitions into it
+def snakeLoop():
+    game_over = False
+
+    x1 = 30
+    y1 = 20
+
+    #it starts by moving up
+    direction = "UP"
+    while not game_over:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+
+            direction = detect_direction(direction, event)
+
+        x1, y1 = move_block(x1, y1, direction)
+
+        SURFACE.fill((0, 0, 0))
+
+        draw_single_block([x1, y1], (0, 255, 0)) #GREEN
+
+        pygame.display.update()
+
+        clock.tick(15)
+
+#definition for drawing the grid (still not working)
+def drawGrid():
+    for x in range(0, TOTAL_COLS, BLOCK_SIZE):
+
+        for y in range(0, TOTAL_ROWS, BLOCK_SIZE):
+
+            rect = pygame.Rect(x, y, BLOCK_SIZE, BLOCK_SIZE)
+
+            pygame.draw.rect(SURFACE, WHITE, rect, 1)
+
+snakeLoop()
+pygame.quit()
